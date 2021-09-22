6126606634428816fd48836ce516b7e79eaed564
HEY <HEY@192.168.55.186>
2021-09-23 02:06:54 +0900

    -

--- firstProject/src/reference/MainExe.java
+++ firstProject/src/reference/MainExe.java
@@ -6,36 +6,71 @@ public class MainExe {
 	
 	public static void main(String[] args) {
 		// 1.학생 수 지정, 2.학생 이름 입력, 3.출력, 4.종료
-		String[] names = null;
+		String[] student = null;
 		while(true) {
 			showMenu();
-			int choice = readInt("번호를 선택하세요.");
-			
+			int choice = readInt("번호를 선택하세요.");		
 			if(choice ==1) {
 				int size = creatStudent();
-				names = new String[size];
+				student = new String[size];
+			}else if(choice ==2) {
+				inputValue(student);
+			}else if(choice ==3) {
+				studentList(student);
+			}else if(choice ==4) {
+				break;
 			}
 		}
-		
+		System.out.println("끝.");
 	}
 	
 	public static void showMenu() {
-		System.out.println("1.학생 수 지정, 2.학생 이름 입력, 3.출력, 4.종료");
-	}
+		System.out.println("1.학생 수 지정, 2.학생 이름 입력, 3.학생 목록, 4.종료");
+	}	
+	
 	
 	public static int creatStudent() {
-		int cnt = readInt("학생 수를 입력하세요.");
+		int cnt = readInt("1. 학생 수를 입력하세요.");
 		return cnt;		
 	}
 	
-	public static void inpuValue(String[] names) {
-		for (String i= 0)
+	public static void inputValue(String[] studentInfo) {
+		if (studentInfo == null) {
+			System.out.println("먼저 학생 수를 입력하세요.");
+			return;
+		}
+		for (int i= 0; i <studentInfo.length; i++) {
+			studentInfo[i]=readString("2. 학생 이름을 입력하세요.");
+		}
 	}
-	
-	public static int readString(String msg) {
+		
+	public static void studentList(String [] studentInfo) {
+		if (studentInfo == null) {
+			System.out.println("먼저 학생 수를 입력하세요.");
+			return;
+		}
+		for (int i=0; i<studentInfo.length; i++) {
+			System.out.printf("%d번 학생의 이름은 %s\n", i, studentInfo[i]);
+		}
+		
+	}
+
+	public static String readString(String msg) {
 		Scanner scn = new Scanner(System.in);
 		System.out.println(msg);
-		String choice = scn.nextLine();
-		return choice;
+		String menu = scn.nextLine();
+		return menu;
 	}
+	 
+	public static int readInt(String msg) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println(msg);
+		int menu = scn.nextInt();
+		return menu;			
+		
+	}
+		
+			
 }
+	
+
